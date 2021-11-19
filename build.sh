f13292bc61ea2e45d944a4cb04ca871c8dfc031d
scottmo <scottscmo@gmail.com>
2021-11-18 20:59:26 -0600

    update build script to build custom jdk

--- build.sh
+++ build.sh
@@ -1,14 +1,40 @@
 #!/usr/bin/env bash
 
+appName="glory-presenter"
+jarName="worship-service-tool-0.0.1-SNAPSHOT.jar"
+mainClass="com.scottscmo.Application"
+imageName="custom-jdk"
+
 type="exe"
 if [ `uname` == "Darwin" ]; then
     type="dmg"
 fi
 
-mvn install
-jpackage --input target/ \
-    --name glory-presenter \
-    --main-jar worship-service-tool-0.0.1-SNAPSHOT.jar \
-    --main-class com.scottscmo.Application \
-    --type $type \
-    --java-options '--enable-preview'
+makeCustomJDK() {
+    jlink --output $imageName \
+        --add-modules java.desktop \
+        --strip-debug --strip-native-commands --compress 2 --no-header-files --no-man-pages
+}
+
+package() {
+    jpackage --input ../target/ \
+        --name $appName \
+        --main-jar $jarName \
+        --main-class $mainClass \
+        --type $type \
+        --java-options '--enable-preview' \
+        --runtime-image $imageName
+}
+
+mkdir -p build
+
+if [ "$0" == "jdk" ]; then
+    cd build
+    makeCustomJDK
+    cd ..
+else
+    mvn install
+    cd build
+    package
+    cd ..
+fi
