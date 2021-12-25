ff11e8e2bb20b1c882ce55dce641303f8a75de5c
scottmo <scottscmo@gmail.com>
2021-12-25 17:06:44 -0600

    change song text field type

--- src/main/java/com/scottscmo/song/Verse.java
+++ src/main/java/com/scottscmo/song/Verse.java
@@ -1,8 +1,17 @@
 package com.scottscmo.song;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record Verse(
-        String verse,
-        Map<String, String[]> text) {
+    String verse,
+    Map<String, String> text
+) {
+    public Map<String, String[]> getText() {
+        return this.text.entrySet().stream()
+            .collect(Collectors.toMap(
+                e -> e.getKey(),
+                e -> e.getValue().split("\n")
+            ));
+    }
 }
