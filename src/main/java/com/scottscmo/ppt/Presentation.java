2b0b3701dbeede82cc7a1175a31b1140f3585199
scottmo <scottscmo@gmail.com>
2022-01-02 23:24:51 -0600

    generate bible slides

--- src/main/java/com/scottscmo/ppt/Presentation.java
+++ src/main/java/com/scottscmo/ppt/Presentation.java
@@ -10,9 +10,8 @@ import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 
 public class Presentation {
-    final static List<TemplateHandler> templateHandlers = List.of(
-        new BibleTemplateHandler(),
-        new SongTemplateHandler());
+    private static final BibleTemplateHandler bibleTemplateHandler = new BibleTemplateHandler();
+    private static final SongTemplateHandler songTemplateHandler = new SongTemplateHandler();
 
     public static void createFromTemplate(String templateFilePath, String outputFilePath) throws IOException {
         try (FileInputStream inStream = new FileInputStream(new File(templateFilePath))) {
@@ -20,9 +19,8 @@ public class Presentation {
             List<XSLFSlide> slides = ppt.getSlides();
 
             for (int i = 0; i < slides.size(); i++) {
-                for (TemplateHandler templateHandler : templateHandlers) {
-                    templateHandler.evaluateTemplate(ppt, slides.get(i).getSlideNumber() - 1);
-                }
+                bibleTemplateHandler.evaluateTemplate(ppt, slides.get(i).getSlideNumber() - 1);
+                songTemplateHandler.evaluateTemplate(ppt, slides.get(i).getSlideNumber() - 1);
             }
 
             // write new PPTX
@@ -33,7 +31,20 @@ public class Presentation {
         }
     }
 
+    public static void insertBibleText(String templateFilePath, String outputFilePath, String bibleReference) throws IOException {
+        try (FileInputStream inStream = new FileInputStream(new File(templateFilePath))) {
+            XMLSlideShow ppt = new XMLSlideShow(inStream);
+            bibleTemplateHandler.insertBibleText(ppt, 1, bibleReference);
+
+            // write new PPTX
+            try (FileOutputStream outStream = new FileOutputStream(new File(outputFilePath))) {
+                ppt.write(outStream);
+            }
+            ppt.close();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        createFromTemplate("template.pptx", "example.pptx");
+        insertBibleText("template.pptx", "psalms 1.pptx", "cuv,niv - psalms 1");
     }
 }
