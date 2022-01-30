c9e3d7047dadba88cd8794ce438d846cad9c7963
Scott Mo <scottscmo@gmail.com>
2022-01-30 16:13:05 -0600

    rename host field

--- src/main/kotlin/com/scottscmo/ui/OutputDisplay.kt
+++ src/main/kotlin/com/scottscmo/ui/OutputDisplay.kt
@@ -4,13 +4,13 @@ import javax.swing.JFrame
 import javax.swing.JOptionPane
 
 object OutputDisplay {
-    var app: JFrame? = null
+    var host: JFrame? = null
 
     fun show(msg: String?) {
-        JOptionPane.showMessageDialog(app, msg);
+        JOptionPane.showMessageDialog(host, msg);
     }
 
     fun error(msg: String?) {
-        JOptionPane.showMessageDialog(app, msg, msg, JOptionPane.ERROR_MESSAGE);
+        JOptionPane.showMessageDialog(host, msg, msg, JOptionPane.ERROR_MESSAGE);
     }
 }
