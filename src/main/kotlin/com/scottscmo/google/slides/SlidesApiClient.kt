0a86db5c53c8b941f59c2ba3df5acb9c426e62ac
Scott Mo <scottscmo@gmail.com>
2022-03-07 16:54:57 -0600

    update slides func

--- src/main/kotlin/com/scottscmo/google/slides/SlidesApiClient.kt
+++ src/main/kotlin/com/scottscmo/google/slides/SlidesApiClient.kt
@@ -2,8 +2,12 @@ package com.scottscmo.google.slides
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.services.slides.v1.Slides
+import com.google.api.services.slides.v1.model.BatchUpdatePresentationRequest
 import com.google.api.services.slides.v1.model.Page
+import com.google.api.services.slides.v1.model.Request
 import com.scottscmo.google.AuthClient
+import java.lang.Integer.min
+
 
 class SlidesApiClient {
     private val appName = "Worship Service Tool"
@@ -16,8 +20,22 @@ class SlidesApiClient {
             .build()
     }
 
-    fun getSlides(presentationId: String): List<Page>? {
+    fun getSlides(presentationId: String): List<Page> {
         val response = service.presentations()[presentationId].execute()
-        return response.slides;
+        return response.slides
+    }
+
+    fun updateSlides(presentationId: String, updateRequests: List<Request>): Boolean {
+        if (updateRequests.isEmpty()) return false
+
+        val batchSize = 500
+        var startIndex = 0
+        while (startIndex < updateRequests.size) {
+            val endIndex = min(startIndex + batchSize, updateRequests.size)
+            val body = BatchUpdatePresentationRequest().setRequests(updateRequests.subList(startIndex, endIndex))
+            service.presentations().batchUpdate(presentationId, body).execute()
+            startIndex = endIndex
+        }
+        return true
     }
 }
