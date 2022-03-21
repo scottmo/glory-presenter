2e0508c094f3d0bb621d61a077be424ab0b518e1
Scott Mo <scottscmo@gmail.com>
2022-03-21 18:52:20 -0500

    add book ids to bible info tab

--- src/main/kotlin/com/scottscmo/ui/panels/BibleInfoPanel.kt
+++ src/main/kotlin/com/scottscmo/ui/panels/BibleInfoPanel.kt
@@ -3,6 +3,7 @@ package com.scottscmo.ui.panels
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.scottscmo.Config
+import com.scottscmo.model.bible.BibleMetadata
 import com.scottscmo.model.bible.BibleModel
 import com.scottscmo.ui.components.Form
 import com.scottscmo.ui.components.FormInput
@@ -10,15 +11,25 @@ import net.miginfocom.swing.MigLayout
 import java.io.File
 import javax.swing.JLabel
 import javax.swing.JPanel
+import javax.swing.JTextArea
 
 class BibleInfoPanel : JPanel() {
     private val mapper = ObjectMapper()
     private val bibleJsonTypeRef = object : TypeReference<Map<String, List<List<String>>>>() {}
     init {
-        layout = MigLayout()
+        layout = MigLayout("", "left", "top")
 
         val availableVersions = BibleModel.get().getAvailableVersions()
-        add(JLabel("Available versions: ${availableVersions.joinToString(", ")}"), "wrap")
+        add(JLabel("Available versions: ${availableVersions.joinToString(", ")}"),
+            "wrap")
+
+        val bookIds = BibleMetadata.getBookIdsInOrder().joinToString(", ")
+        add(JTextArea("Book IDs: $bookIds").apply {
+            columns = 100
+            isEditable = false
+            lineWrap = true
+            wrapStyleWord = true
+        }, "span, wrap")
 
         val dataPathKey = "dataFilePath"
         val versionKey = "version"
--- src/main/kotlin/com/scottscmo/ui/panels/SlidesGeneratorsPanel.kt
+++ src/main/kotlin/com/scottscmo/ui/panels/SlidesGeneratorsPanel.kt
@@ -46,7 +46,7 @@ class SlidesGeneratorsPanel : JPanel() {
         val versesKey = "verses"
         val versionsKey = "versions"
         add(Form("Bible Slides Generator", mapOf(
-            versesKey to FormInput("Verses", "text"),
+            versesKey to FormInput("Verses", "text", "john 1:2-5,7-8"),
             versionsKey to FormInput("Bible Versions", "text", "cuv,niv"),
             templatePathKey to FormInput("Template File", "file", Config.getRelativePath("template-bible.pptx")),
             outputDirKey to outputDirDefault
