d9d69cd61c4ed65be9185541f193505e1ebff34d
Scott Mo <scottscmo@gmail.com>
2022-06-11 19:18:50 -0500

    kvmd stringify

--- src/main/kotlin/com/scottscmo/util/KVMD.kt
+++ src/main/kotlin/com/scottscmo/util/KVMD.kt
@@ -1,11 +1,28 @@
 package com.scottscmo.util
 
 object KVMD {
+    private const val KEY_NAMESPACE = "namespace"
+    private const val KEY_METADATA = "metadata"
+    private const val KEY_CONTENT = "content"
+
     private const val SECTION_DELIMITER = "==="
 
     private const val ERR_MSG_INVALID_FORMAT = "KVMD.parse: Invalid input format!"
-    
+
+    fun getNamespace(obj: Map<String, Any>): String {
+        return obj[KEY_NAMESPACE] as String
+    }
+
+    fun getMetadata(obj: Map<String, Any>): Map<String, Any> {
+        return (obj[KEY_METADATA] ?: mapOf<String, Any>()) as Map<String, Any>
+    }
+
+    fun getContent(obj: Map<String, Any>): Map<String, Any> {
+        return (obj[KEY_CONTENT] ?: mapOf<String, Any>()) as Map<String, Any>
+    }
+
     fun parse(input: String): Map<String, Any> {
+        val namespace: String
         val metadata: Map<String, Any>
         val content: Map<String, Any>
 
@@ -14,20 +31,23 @@ object KVMD {
 
         when (sections.size) {
             1 -> { // content
+                namespace = ""
                 metadata = mapOf()
                 content = parseContent(sections[0])
             }
             2 -> { // metadata, content
+                namespace = ""
                 metadata = parseMetadata(sections[0])
                 content = parseContent(sections[1])
             }
             else -> { // name, metadata, content
-                metadata = mapOf("name" to sections[0]) + parseMetadata(sections[1])
+                namespace = sections[0]
+                metadata = parseMetadata(sections[1])
                 content = parseContent(sections[2])
             }
         }
 
-        return mapOf("metadata" to metadata, "content" to content)
+        return mapOf(KEY_NAMESPACE to namespace, KEY_METADATA to metadata, KEY_CONTENT to content)
     }
 
     private fun parseMetadata(input: String): Map<String, Any> {
@@ -50,7 +70,7 @@ object KVMD {
     private fun parseContent(input: String, prefix: String = "#", level: Int = 1): Map<String, Any> {
         // make sure we have the prefix at the very least
         val keyRegex = "(^|\n)$prefix".toRegex()
-        if (!keyRegex.matches(input)) return mapOf()
+        if (!keyRegex.containsMatchIn(input)) return mapOf()
 
         // find all the keys and recursively parse the values
         val currentLevelkeyRegex = "(^|\n)$prefix{$level}\\s(.+)($|\n)".toRegex()
@@ -66,13 +86,52 @@ object KVMD {
         return keys.zip(values).toMap()
     }
 
-    fun stringify(obj: Map<String, Map<String, Any>>): String {
-        return ""
+    /**
+     * Serialize object to KVMD format. Should be in the following structure:
+     * {
+     *     metadata: {
+     *         [key: String]: String | Array
+     *     },
+     *     content: {
+     *         [key: String]: String | Map
+     *     }
+     * }
+     */
+    fun stringify(obj: Map<String, Any>): String {
+        val namespace = getNamespace(obj)
+        val metadataStr = getMetadata(obj).entries
+                .sortedBy { it.key }
+                .joinToString("\n") { it.key + ": " + stringifyMetadataValue(it.value) }
+        val contentStr = stringifyContent(getContent(obj))
+
+        return listOf(namespace, metadataStr, contentStr)
+            .filter { it.isNotEmpty() }
+            .joinToString("\n$SECTION_DELIMITER\n")
+    }
+
+    private fun stringifyMetadataValue(value: Any): String {
+        if (value is List<*>) {
+            return "[" + value.joinToString(",") + "]"
+        }
+        return value.toString()
+    }
+
+    private fun stringifyContent(content: Map<String, Any>, prefix: String = "#", level: Int = 1): String {
+        return content.entries
+            .sortedBy { it.key }
+            .joinToString("\n") {
+                val key = "\n" + prefix.repeat(level) + " " + it.key
+                if (it.value is Map<*, *>) {
+                    key + "\n" + stringifyContent(it.value as Map<String, Any>, prefix, level + 1)
+                } else {
+                    key + "\n" + it.value
+                }
+            }
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println(parse("""
+        val parsed = parse("""
 獻上感恩 Give Thanks
 ===
 tags: Henry Smith
@@ -80,9 +139,11 @@ order: [v1, v2, v1, v2]
 ===
 
 # v1
+
 ## zh
 献上感恩的心
 归给至圣全能神
+
 ## en
 Give thanks with a grateful heart
 give thanks to the holy One.
@@ -94,6 +155,9 @@ give thanks to the holy One.
 ## en
 And now let the weak say I am strong
 let the poor say I am rich
-        """.trimIndent()))
+        """.trimIndent())
+        println(parsed)
+        val parsedStr = stringify(parsed)
+        println(parsedStr)
     }
 }
