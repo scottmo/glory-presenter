8613b42ba6091e13d1c7eb5a23f8ae3b044a921b
scottmo <scottscmo@gmail.com>
2022-10-13 18:47:54 -0500

    feat: add logger

--- src/main/kotlin/com/scottscmo/Application.java
+++ src/main/kotlin/com/scottscmo/Application.java
@@ -8,11 +8,18 @@ import com.scottscmo.ui.panels.PPTXGeneratorsPanel;
 import com.scottscmo.ui.panels.SettingsPanel;
 import com.scottscmo.ui.panels.SongFormatterPanel;
 import net.miginfocom.swing.MigLayout;
+import org.apache.log4j.Logger;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Application extends JFrame {
+
+    static Logger logger = Logger.getLogger(Application.class.getName());
+    private static Application _app;
+    public static Application get() {
+        return _app;
+    }
 
     public Application() {
         setTitle("Worship Service Tool");
@@ -36,12 +43,12 @@ public class Application extends JFrame {
         pack(); // auto-resize to component, use setSize if need fixed size
     }
 
-    private static Application _app;
-    public static Application get() {
-        return _app;
-    }
-
     public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler((Thread t, Throwable e) -> {
+            logger.error("Uncaught exception in thread: " + t.getName(), e);
+            e.printStackTrace();
+        });
+
         Config.reload();
         FlatLightLaf.setup();
         SwingUtilities.invokeLater(() -> {
