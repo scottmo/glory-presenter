444dc455fa9a5acfb780a19277b1a8a6f6154cb8
scottmo <scottscmo@gmail.com>
2022-11-02 14:06:51 -0500

    feat: attach some handlers and update layouts

--- src/main/java/com/scottmo/controllers/VerseEditorController.java
+++ src/main/java/com/scottmo/controllers/VerseEditorController.java
@@ -1,10 +1,42 @@
 package com.scottmo.controllers;
 
+import com.scottmo.services.openLyrics.OpenLyrics;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class VerseEditorController {
-    @FXML protected void handleAddVerse(ActionEvent event) {
+    public TextField titleInput;
+    public ScrollPane lyricsContainer;
+    public TextField verseOrderInput;
+    public ComboBox verseOrderPicker;
+
+    private OpenLyrics song;
+
+    public void initialize() {
+        Stage stage = (Stage) titleInput.getScene().getWindow();
+        song = (OpenLyrics) stage.getUserData();
+    }
+
+    protected void handleAddVerse(ActionEvent event) {
         System.out.println("test");
+    }
+
+    public void onAddVerse(ActionEvent event) {
+    }
+
+    public void onEditVerse(ActionEvent event) {
+    }
+
+    public void onDeleteVerse(ActionEvent event) {
+    }
+
+    public void onCancel(ActionEvent event) {
+    }
+
+    public void onSave(ActionEvent event) {
     }
 }
