6d33b686a3eac5bee9b0066c693214856599b697
scottmo <scottscmo@gmail.com>
2022-11-13 19:14:19 -0600

    feat: song store WIP

--- src/main/java/com/scottmo/services/songs/SongStore.java
+++ src/main/java/com/scottmo/services/songs/SongStore.java
@@ -1,5 +1,7 @@
 package com.scottmo.services.songs;
 
+import com.scottmo.data.song.Song;
+
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,13 +10,27 @@ import java.sql.SQLException;
 public final class SongStore {
     private static final String DB_NAME = "songs";
     private final SongTable songTable;
+    private final SongTitleTable songTitleTable;
+    private final SongVerseTable songVerseTable;
 
     public SongStore(Path storeLocation) {
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:" + storeLocation.resolve("%s.db".formatted(DB_NAME)));
             songTable = new SongTable(conn);
+            songTitleTable = new SongTitleTable(conn);
+            songVerseTable = new SongVerseTable(conn);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean insert(Song song) {
+        try {
+            long songId = songTable.insert(song);
+        } catch (SQLException e) {
+
+        }
+
+        return false;
     }
 }
--- src/main/java/com/scottmo/services/songs/SongTable.java
+++ src/main/java/com/scottmo/services/songs/SongTable.java
@@ -1,23 +1,29 @@
 package com.scottmo.services.songs;
 
+import com.scottmo.data.song.Song;
+import javafx.util.Pair;
+import org.apache.logging.log4j.util.Strings;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 final class SongTable {
-    private static final String MAIN_TABLE = "songs";
-    private static final String VERSES_TABLE = "verses";
-    private static final String TITLES_TABLE = "titles";
+    private static final String TABLE_NAME = "songs";
 
     private final Connection db;
 
-    public SongTable(Connection conn) {
+    SongTable(Connection conn) throws SQLException {
         this.db = conn;
+        createTable();
     }
 
     private void createTable() throws SQLException {
-        List<String> sqls = List.of("""
+        String sql = """
                 CREATE TABLE IF NOT EXISTS %s (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     authors TEXT,
@@ -27,33 +33,61 @@ final class SongTable {
                     entry TEXT,
                     comments TEXT,
                     verseOrder TEXT
-                )""".formatted(MAIN_TABLE),
-                """
-                CREATE TABLE IF NOT EXISTS %s (
-                    song_id INTEGER NOT NULL,
-                    name TEXT NOT NULL,
-                    locale TEXT NOT NULL,
-                    text TEXT NOT NULL,
-                    PRIMARY KEY(song_id, name, locale),
-                    FOREIGN KEY(song_id) REFERENCES songs(id)
-                )""".formatted(VERSES_TABLE),
-                """
-                CREATE TABLE IF NOT EXISTS %s (
-                    song_id INTEGER,
-                    locale TEXT NOT NULL,
-                    text TEXT NOT NULL,
-                    PRIMARY KEY(song_id, locale),
-                    FOREIGN KEY(song_id) REFERENCES songs(id)
-                )""".formatted(TITLES_TABLE)
-        );
+                )""".formatted(TABLE_NAME);
         try (Statement stmt = db.createStatement()) {
-            for (String sql : sqls) {
-                stmt.executeUpdate(sql);
-            }
+            stmt.executeUpdate(sql);
         }
     }
 
-    public int insert() {
-        return 0;
+    Song getSong(int id) {
+        Song song = new Song();
+
+        return song;
+    }
+
+    long insert(Song song) throws SQLException {
+        List<Pair<String, String>> nonEmptyFields = new ArrayList<>();
+        if (!song.getAuthors().isEmpty()) {
+            nonEmptyFields.add(new Pair<>("authors", String.join(", ", song.getAuthors())));
+        }
+        if (Strings.isNotEmpty(song.getCopyright())) {
+            nonEmptyFields.add(new Pair<>("copyright", song.getCopyright()));
+        }
+        if (Strings.isNotEmpty(song.getPublisher())) {
+            nonEmptyFields.add(new Pair<>("publisher", song.getPublisher()));
+        }
+        if (Strings.isNotEmpty(song.getSongBook())) {
+            nonEmptyFields.add(new Pair<>("songbook", song.getSongBook()));
+        }
+        if (Strings.isNotEmpty(song.getEntry())) {
+            nonEmptyFields.add(new Pair<>("entry", song.getEntry()));
+        }
+        if (Strings.isNotEmpty(song.getComments())) {
+            nonEmptyFields.add(new Pair<>("comments", song.getComments()));
+        }
+        if (!song.getVerseOrder().isEmpty()) {
+            nonEmptyFields.add(new Pair<>("verseOrder", String.join(" ", song.getVerseOrder())));
+        }
+        long songId;
+        String sql = "INSERT INTO %s (%s) VALUES (%s)".formatted(
+                TABLE_NAME,
+                String.join(", ", nonEmptyFields.stream().map(Pair::getKey).toList()),
+                Strings.repeat("?", nonEmptyFields.size())
+        );
+        try (PreparedStatement stmt = db.prepareStatement(sql)) {
+            for (int i = 0; i < nonEmptyFields.size(); i++) {
+                var field = nonEmptyFields.get(i);
+                stmt.setString(i + 1, field.getValue());
+            }
+            stmt.executeUpdate();
+
+            ResultSet res = stmt.getGeneratedKeys();
+            if (res.next()) {
+                songId = res.getLong(1);
+            } else {
+                throw new RuntimeException("Failed to retrieve id from new song");
+            }
+        }
+        return songId;
     }
 }
