b69a3606d57a69479908e9df72c75d3d52f8a46e
scottmo <scottscmo@gmail.com>
2022-11-14 16:56:11 -0600

    refactor: change verses to a list

--- src/main/java/com/scottmo/data/song/SongVerse.java
+++ src/main/java/com/scottmo/data/song/SongVerse.java
@@ -4,11 +4,14 @@ public class SongVerse {
     private String name;
     private String text;
 
+    private String locale;
+
     public SongVerse() {}
 
-    public SongVerse(String name, String text) {
+    public SongVerse(String name, String text, String locale) {
         this.name = name;
         this.text = text;
+        this.locale = locale;
     }
 
     public String getName() {
@@ -23,5 +26,13 @@ public class SongVerse {
     }
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
     }
 }
