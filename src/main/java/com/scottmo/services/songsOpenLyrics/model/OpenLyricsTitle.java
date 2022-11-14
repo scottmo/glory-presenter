bc78d7219745919fe495b26c8f22d452aa2100de
scottmo <scottscmo@gmail.com>
2022-11-14 16:13:47 -0600

    refactor: switch to a more flexible OL song parser

---
+++ src/main/java/com/scottmo/services/songsOpenLyrics/model/OpenLyricsTitle.java
@@ -0,0 +1,38 @@
+package com.scottmo.services.songsOpenLyrics.model;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlText;
+
+public class OpenLyricsTitle {
+    @JacksonXmlProperty(isAttribute = true)
+    private String lang;
+    @JacksonXmlText
+    private String value;
+
+    public OpenLyricsTitle() {}
+
+    public OpenLyricsTitle(String value) {
+        this.value = value;
+    }
+
+    public OpenLyricsTitle(String lang, String value) {
+        this.lang = lang;
+        this.value = value;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
