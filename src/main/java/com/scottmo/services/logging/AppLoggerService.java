70fc680dafc3f328c316bffcf0cf5a05b40b5816
Scott Mo 莫少 <scottmo@users.noreply.github.com>
2022-11-27 20:55:58 +0000

    fix: imports

--- src/main/java/com/scottmo/services/logging/AppLoggerService.java
+++ src/main/java/com/scottmo/services/logging/AppLoggerService.java
@@ -1,5 +1,7 @@
 package com.scottmo.services.logging;
 
+import com.scottmo.services.Service;
+
 public class AppLoggerService implements Service {
     private AppLogger appLogger;
     public void registerLogger(AppLogger logger) {
