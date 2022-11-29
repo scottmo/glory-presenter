5270a051a16ed46bd16acf837592b32c49fd3d5a
scottmo <scottscmo@gmail.com>
2022-11-29 14:39:32 -0600

    feat: display all song titles

--- src/main/java/com/scottmo/app/controllers/SongViewerController.java
+++ src/main/java/com/scottmo/app/controllers/SongViewerController.java
@@ -1,17 +1,59 @@
 package com.scottmo.app.controllers;
 
 import com.scottmo.app.views.ViewUtil;
+import com.scottmo.services.ServiceSupplier;
+import com.scottmo.services.songs.SongService;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static com.scottmo.config.Constants.PRIMARY_LOCALE;
+import static com.scottmo.config.Constants.SECONDARY_LOCALE;
 
 public class SongViewerController {
     private static final String VERSE_EDITOR_FXML = "/ui/songEditor.fxml";
+
+    private Supplier<SongService> songService = ServiceSupplier.get(SongService.class);
+
+    public VBox songList;
+
+    public void initialize() {
+        Platform.runLater(this::initSongList);
+    }
+
+    private void initSongList() {
+        Map<Integer, String> titles = new HashMap<>();
+        for (var title : songService.get().getStore().getTitles(PRIMARY_LOCALE)) {
+            titles.put(title.getKey(), title.getValue());
+        }
+        for (var title : songService.get().getStore().getTitles(SECONDARY_LOCALE)) {
+            var songId = title.getKey();
+            if (titles.containsKey(songId)) {
+                titles.put(songId, titles.get(songId) + " | " + title.getValue());
+            } else {
+                titles.put(songId, title.getValue());
+            }
+        }
+        titles.forEach((key, value) -> {
+            Label titleLabel = new Label(value);
+            titleLabel.getProperties().put("id", key);
+            songList.getChildren().add(titleLabel);
+        });
+    }
 
     public void onEditSong(ActionEvent event) throws IOException, ParserConfigurationException, SAXException {
         Stage verseEditorModal = ViewUtil.get().newModal("Edit Song", VERSE_EDITOR_FXML, ViewUtil.get().getOwnerWindow(event));
--- src/main/java/com/scottmo/config/Constants.java
+++ src/main/java/com/scottmo/config/Constants.java
@@ -1,5 +1,11 @@
 package com.scottmo.config;
 
+import java.util.Locale;
+
 public class Constants {
     public static final String APP_NAME = "Glory Presenter";
+
+    // TODO: make this configurable
+    public static final String PRIMARY_LOCALE = Locale.getDefault().toString();
+    public static final String SECONDARY_LOCALE = new Locale("zh_CN").toString();
 }
