23a1fd11f61f3b4a20d7d377fd1cd44483cc4d15
scottmo <scottscmo@gmail.com>
2022-12-31 23:40:16 -0600

    fix: update

--- src/main/java/com/scottmo/services/songs/store/SongStore.java
+++ src/main/java/com/scottmo/services/songs/store/SongStore.java
@@ -112,10 +112,7 @@ public final class SongStore {
 
     public boolean store(Song song) {
         if (song.getId() > -1) {
-            // return update(song);
-            // for update, for now we just remove the song and reinsert it
-            // since titles and verses are very hard to update
-            delete(song.getId());
+             return update(song);
         }
         return insert(song);
     }
@@ -169,88 +166,67 @@ public final class SongStore {
                 }
             }
 
-            sql = new InsertQuery(schema.titles.table)
-                    .addPreparedColumns(schema.titles.songId, schema.titles.locale, schema.titles.text);
-            sql.validate();
-            try (var stmt = db.prepareStatement(sql.toString())) {
-                for (String locale : song.getLocales()) {
-                    stmt.setInt(1, songId);
-                    stmt.setString(2, locale);
-                    stmt.setString(3, song.getTitle(locale));
-
-                    stmt.addBatch();
-                }
-                stmt.executeBatch();
-            }
-
-            sql = new InsertQuery(schema.verses.table)
-                    .addPreparedColumns(schema.verses.songId, schema.verses.name, schema.verses.text, schema.verses.locale);
-            sql.validate();
-            try (var stmt = db.prepareStatement(sql.toString())) {
-                for (SongVerse verse : song.getVerses()) {
-                    stmt.setInt(1, songId);
-                    stmt.setString(2, verse.getName());
-                    stmt.setString(3, verse.getText());
-                    stmt.setString(4, verse.getLocale());
-
-                    stmt.addBatch();
-                }
-                stmt.executeBatch();
-            }
+            insertTitles(songId, song);
+            insertVerses(songId, song);
         } catch (SQLException e) {
             throw new RuntimeException("Unable to insert song to DB!", e);
         }
         return true;
     }
 
-    private boolean update(Song song) {
-        try (var stmt = db.createStatement()) {
-            UpdateQuery sql = new UpdateQuery(schema.song.table);
-            sql.addCondition(BinaryCondition.equalTo(schema.song.id, song.getId()));
-            if (!song.getAuthors().isEmpty()) {
-                sql.addSetClause(schema.song.authors, song.getAuthors());
-            }
-            if (Strings.isNotEmpty(song.getCopyright())) {
-                sql.addSetClause(schema.song.copyright, song.getCopyright());
-            }
-            if (Strings.isNotEmpty(song.getPublisher())) {
-                sql.addSetClause(schema.song.publisher, song.getPublisher());
-            }
-            if (Strings.isNotEmpty(song.getSongBook())) {
-                sql.addSetClause(schema.song.songbook, song.getSongBook());
-            }
-            if (Strings.isNotEmpty(song.getEntry())) {
-                sql.addSetClause(schema.song.entry, song.getEntry());
-            }
-            if (Strings.isNotEmpty(song.getComments())) {
-                sql.addSetClause(schema.song.comments, song.getComments());
-            }
-            if (Strings.isNotEmpty(song.getVerseOrder())) {
-                sql.addSetClause(schema.song.verseOrder, song.getVerseOrder());
-            }
-            sql.validate();
-            stmt.addBatch(sql.toString());
-
+    private void insertTitles(int songId, Song song) throws SQLException {
+        var sql = new InsertQuery(schema.titles.table)
+                .addPreparedColumns(schema.titles.songId, schema.titles.locale, schema.titles.text);
+        sql.validate();
+        try (var stmt = db.prepareStatement(sql.toString())) {
             for (String locale : song.getLocales()) {
-                sql = new UpdateQuery(schema.titles.table)
-                        .addCondition(BinaryCondition.equalTo(schema.titles.songId, song.getId()))
-                        .addSetClause(schema.titles.locale, locale)
-                        .addSetClause(schema.titles.text, song.getTitle(locale))
-                        .validate();
-                stmt.addBatch(sql.toString());
-            }
+                stmt.setInt(1, songId);
+                stmt.setString(2, locale);
+                stmt.setString(3, song.getTitle(locale));
 
-            for (SongVerse verse : song.getVerses()) {
-                sql = new UpdateQuery(schema.verses.table)
-                        .addCondition(BinaryCondition.equalTo(schema.verses.songId, song.getId()))
-                        .addSetClause(schema.verses.name, verse.getName())
-                        .addSetClause(schema.verses.text, verse.getText())
-                        .addSetClause(schema.verses.locale, verse.getLocale())
-                        .validate();
-                stmt.addBatch(sql.toString());
+                stmt.addBatch();
             }
-
             stmt.executeBatch();
+        }
+    }
+
+    private void insertVerses(int songId, Song song) throws SQLException {
+        var sql = new InsertQuery(schema.verses.table)
+                .addPreparedColumns(schema.verses.songId, schema.verses.name, schema.verses.text, schema.verses.locale);
+        sql.validate();
+        try (var stmt = db.prepareStatement(sql.toString())) {
+            for (SongVerse verse : song.getVerses()) {
+                stmt.setInt(1, songId);
+                stmt.setString(2, verse.getName());
+                stmt.setString(3, verse.getText());
+                stmt.setString(4, verse.getLocale());
+
+                stmt.addBatch();
+            }
+            stmt.executeBatch();
+        }
+    }
+
+    private boolean update(Song song) {
+        var locales = song.getLocales();
+        if (!locales.isEmpty()) {
+            delete(song.getId(), true);
+        }
+        try (var stmt = db.createStatement()) {
+            UpdateQuery sql = new UpdateQuery(schema.song.table)
+                .addCondition(BinaryCondition.equalTo(schema.song.id, song.getId()))
+                .addSetClause(schema.song.authors, String.join(",", song.getAuthors()))
+                .addSetClause(schema.song.copyright, song.getCopyright())
+                .addSetClause(schema.song.publisher, song.getPublisher())
+                .addSetClause(schema.song.songbook, song.getSongBook())
+                .addSetClause(schema.song.entry, song.getEntry())
+                .addSetClause(schema.song.comments, song.getComments())
+                .addSetClause(schema.song.verseOrder, song.getVerseOrder())
+                .validate();
+            stmt.executeUpdate(sql.toString());
+
+            insertTitles(song.getId(), song);
+            insertVerses(song.getId(), song);
         } catch (SQLException e) {
             throw new RuntimeException("Unable to update song!", e);
         }
@@ -258,6 +234,10 @@ public final class SongStore {
     }
 
     public boolean delete(int songId) {
+        return delete(songId, false);
+    }
+
+    private boolean delete(int songId, boolean preserveMetadata) {
         var deleteSong = new DeleteQuery(schema.song.table)
                 .addCondition(BinaryCondition.equalTo(schema.song.id, songId))
                 .validate().toString();
@@ -270,7 +250,9 @@ public final class SongStore {
         try (var stmt = db.createStatement()) {
             stmt.addBatch(deleteTitles);
             stmt.addBatch(deleteVerses);
-            stmt.addBatch(deleteSong);
+            if (!preserveMetadata) {
+                stmt.addBatch(deleteSong);
+            }
             stmt.executeBatch();
         } catch (SQLException e) {
             throw new RuntimeException("Unable to delete song!", e);
