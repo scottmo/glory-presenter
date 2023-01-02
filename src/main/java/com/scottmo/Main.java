add164664d17d7cf430685c11f29b680195e63b7
scottmo <scottscmo@gmail.com>
2023-01-02 14:35:05 -0600

    feat: change build setup - javafx and java version to 17 - jpackage/jlink to just fat jar

---
+++ src/main/java/com/scottmo/Main.java
@@ -0,0 +1,11 @@
+package com.scottmo;
+
+import com.scottmo.app.App;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // workaround to make fat jar executable
+        App.main(args);
+    }
+}
