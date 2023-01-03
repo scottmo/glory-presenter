5cbf304bc5fa0ae07f25a510bac65b97eada2f71
scottmo <scottscmo@gmail.com>
2023-01-03 12:08:37 -0600

    refactor: small cleanup

--- src/main/java/com/scottmo/app/controllers/MainController.java
+++ src/main/java/com/scottmo/app/controllers/MainController.java
@@ -1,9 +1,5 @@
 package com.scottmo.app.controllers;
 
-import atlantafx.base.theme.PrimerDark;
-import atlantafx.base.theme.PrimerLight;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -11,15 +7,4 @@ public class MainController {
 
     @FXML
     private Label statusLabel;
-    // menu
-    private boolean isDark;
-    @FXML
-    private void onToggleDarkMode(ActionEvent actionEvent) {
-        if (isDark) {
-            Application.setUserAgentStylesheet(new PrimerLight().getUserAgentStylesheet());
-        } else {
-            Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
-        }
-        isDark = !isDark;
-    }
 }
