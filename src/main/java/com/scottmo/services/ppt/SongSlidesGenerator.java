64ff0b42b9ba020eb5a2bcde342eff69f91454de
scottmo <scottscmo@gmail.com>
2023-01-22 22:11:11 -0600

    feat: generate pptx from song

---
+++ src/main/java/com/scottmo/services/ppt/SongSlidesGenerator.java
@@ -0,0 +1,133 @@
+package com.scottmo.services.ppt;
+
+import com.scottmo.data.song.Song;
+import com.scottmo.data.song.SongVerse;
+import com.scottmo.util.StringUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class SongSlidesGenerator {
+
+    // placeholder keys
+    private static final String VERSE_PREFIX = "verse_";
+    private static final String TITLE_PREFIX = "title_";
+    private static final String SONGBOOK = "songbook";
+    private static final String ENTRY = "entry";
+    private static final String COPYRIGHT = "copyright";
+    private static final String PUBLISHER = "publisher";
+    private static final String PLACEHOLDER_TEMPLATE = "{%s}";
+
+    public static void generate(Song song, String tmplFilePath, String outputFilePath, List<String> locales, int maxLines) throws IOException {
+        List<Map<String, String>> slideContents = new ArrayList<>();
+
+        // song metadata for all slides
+        Map<String, String> songMetadata = new HashMap<>();
+        songMetadata.put(SONGBOOK, song.getSongBook());
+        songMetadata.put(ENTRY, song.getEntry());
+        songMetadata.put(COPYRIGHT, song.getCopyright());
+        songMetadata.put(PUBLISHER, song.getPublisher());
+        for (String locale: song.getLocales()) {
+            songMetadata.put(TITLE_PREFIX + locale, song.getTitle(locale));
+        }
+        // title
+        slideContents.add(songMetadata); // add only song metadata for title
+
+        // lyrics
+        List<String> textGroups = locales.stream().map(s -> VERSE_PREFIX + s).toList();
+        List<Map<String, String>> lyrics = getSectionTexts(getOrderedVerses(song), textGroups, maxLines);
+        // make song metadata also available to verse slides
+        for (int i = 0; i < song.getVerseOrder().size(); i++) {
+            Map<String, String> verse = lyrics.get(i);
+            verse.putAll(songMetadata);
+            verse.put("verseName", song.getVerseOrder().get(i));
+        }
+        slideContents.addAll(lyrics);
+
+        TemplatingUtil.generateSlideShow(slideContents, PLACEHOLDER_TEMPLATE, tmplFilePath, outputFilePath);
+    }
+
+    /**
+     *
+     * @return
+     * [
+     *     {
+     *         [locale]: [
+     *             "verse1line1",
+     *             "verse1line2",
+     *             ...
+     *          ]
+     *     }
+     * ]
+     */
+    public static List<Map<String, List<String>>> getOrderedVerses(Song song) {
+        List<Map<String, List<String>>> orderedVerses = new ArrayList<>(song.getVerseOrder().size());
+        for (int i = 0; i < song.getVerseOrder().size(); i++) {
+            orderedVerses.add(new HashMap<>());
+        }
+
+        Map<String, Integer> verseOrderMap = new HashMap<>();
+        for (int i = 0; i < song.getVerseOrder().size(); i++) {
+            verseOrderMap.put(song.getVerseOrder().get(i), i);
+        }
+        for (SongVerse verse : song.getVerses()) {
+            Map<String, List<String>> verseGroup = orderedVerses.get(verseOrderMap.get(verse.getName()));
+            verseGroup.put(VERSE_PREFIX + verse.getLocale(), StringUtils.split(verse.getText(), "\n"));
+        }
+
+        return orderedVerses;
+    }
+
+    private static int getValueListSize(Map<String, List<String>> map) {
+        return map.values().stream().toList().get(0).size();
+    }
+
+    private static Map<String, List<String>> getSectionTextsByGroup(List<Map<String, List<String>>> orderedSectionTexts, List<String> textGroups, int maxLines) {
+        if (textGroups.isEmpty()) {
+            return Collections.emptyMap();
+        }
+        Map<String, List<String>> data = new HashMap<>();
+        for (Map<String, List<String>> sectionTexts : orderedSectionTexts) {
+            // assuming all langs have same # of section lines
+            int numSectionLines = getValueListSize(sectionTexts);
+            int numSlidePerThisSection = 0;
+            while (numSlidePerThisSection * maxLines < numSectionLines) {
+                for (String group : textGroups) {
+                    List<String> lines = data.getOrDefault(group, new ArrayList<>());
+                    String line = "";
+                    List<String> sectionLines = sectionTexts.get(group);
+                    if (sectionLines != null && !sectionLines.isEmpty()) {
+                        for (int i = 0; i < maxLines; i++) {
+                            int currLineInSection = numSlidePerThisSection * maxLines + i;
+                            if (currLineInSection < sectionLines.size()) {
+                                line += sectionLines.get(currLineInSection) + "\n";
+                            }
+                        }
+                        lines.add(line.trim());
+                        data.put(group, lines);
+                    }
+                }
+                numSlidePerThisSection++;
+            }
+        }
+        return data;
+    }
+
+    public static List<Map<String, String>> getSectionTexts(List<Map<String, List<String>>> orderedSectionTexts, List<String> textGroups, int maxLines) {
+        Map<String, List<String>> distributedText = getSectionTextsByGroup(orderedSectionTexts, textGroups, maxLines);
+        int numSections = getValueListSize(distributedText);
+        return IntStream.range(0, numSections)
+                .mapToObj(i -> textGroups.stream()
+                        .filter(distributedText::containsKey)
+                        .collect(Collectors.toMap(
+                                group -> group,
+                                group -> distributedText.get(group).get(i))))
+                .collect(Collectors.toList());
+    }
+}
--- src/main/java/com/scottmo/services/ppt/TemplatingUtil.java
+++ src/main/java/com/scottmo/services/ppt/TemplatingUtil.java
@@ -7,6 +7,10 @@ import org.apache.poi.xslf.usermodel.XSLFSlideMaster;
 import org.apache.poi.xslf.usermodel.XSLFTextParagraph;
 import org.apache.poi.xslf.usermodel.XSLFTextShape;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -77,8 +81,7 @@ final class TemplatingUtil {
      * XSLFTextShape.setText("") is bugged when there's new line
      */
     public static void clearText(XSLFTextShape textShape) {
-        textShape.getTextParagraphs().stream()
-                .forEach(TemplatingUtil::clearText);
+        textShape.getTextParagraphs().forEach(TemplatingUtil::clearText);
     }
 
     public static void clearText(XSLFTextParagraph pp) {
@@ -133,5 +136,87 @@ final class TemplatingUtil {
         return Optional.ofNullable(getSlideMaster(ppt, name))
                 .map(master -> master.getLayout(layout))
                 .orElse(null);
+    }
+
+    private static void duplicateSlide(XMLSlideShow slides, XSLFSlide srcSlide) {
+        var slide = slides.createSlide(srcSlide.getSlideLayout());
+        slide.importContent(srcSlide);
+    }
+
+    /**
+     * Generate PPTX file based on list of k,v maps and a template file.
+     * Template file can have up to 3 template slides:
+     * 1. start or content slide
+     * 2. content slide
+     * 3. end slide
+     * Content slide will contain the repeated k,v maps.
+     * Note that currently we cannot have multiple styles for different placeholders in the same line. Placeholders
+     * need to be delimited by a new line in order of have different styles.
+     *
+     * @param contents k,v placeholder value maps, each map represent 1 slide.
+     *                 First k,v map will be used as start slide if present.
+     *                 Last k,v map will be used as end slide if present.
+     * @param placeholderTemplate placeholder format string. e.g. {%s}
+     * @param tmplFilePath template pptx
+     * @param outputFilePath output pptx
+     * @throws IOException
+     */
+    public static void generateSlideShow(List<Map<String, String>> contents, String placeholderTemplate,
+                                         String tmplFilePath, String outputFilePath) throws IOException {
+        // make copies of template slides first and write
+        // since for some reason the pptx reference is not able to modify the new copies in memory
+        try (var inStream = new FileInputStream(tmplFilePath)) {
+            var tmplSlides = new XMLSlideShow(inStream);
+
+            var numPlaceholderSlides = tmplSlides.getSlides().size();
+            var hasStartSlide = numPlaceholderSlides > 1;
+            var hasEndSlide = numPlaceholderSlides > 2;
+
+            // start slide if present
+            XSLFSlide srcSlide;
+            if (hasStartSlide) {
+                srcSlide = tmplSlides.getSlides().get(0);
+                duplicateSlide(tmplSlides, srcSlide);
+            }
+
+            // content slides
+            int numContentSlides = contents.size();
+            if (hasStartSlide) numContentSlides--;
+            if (hasEndSlide) numContentSlides--;
+            srcSlide = tmplSlides.getSlides().get(1);
+            for (int j = 0; j < numContentSlides; j++) {
+                duplicateSlide(tmplSlides, srcSlide);
+            }
+
+            // end slide if present
+            if (hasEndSlide) {
+                srcSlide = tmplSlides.getSlides().get(2);
+                duplicateSlide(tmplSlides, srcSlide);
+            }
+
+            // remove template slides
+            for (int i = 0; i < numPlaceholderSlides; i++) {
+                tmplSlides.removeSlide(0);
+            }
+            try (var outStream = new FileOutputStream(outputFilePath)) {
+                tmplSlides.write(outStream);
+            }
+            tmplSlides.close();
+        }
+
+        // now do the modifications
+        try (var inStream = new FileInputStream(outputFilePath)) {
+            var ppt = new XMLSlideShow(inStream);
+            for (int i = 0; i < contents.size(); i++) {
+                var slide = ppt.getSlides().get(i);
+                Map<String, String> values = contents.get(i).entrySet().stream()
+                                .collect(Collectors.toMap(e -> placeholderTemplate.formatted(e.getKey()), e -> e.getValue()));
+                TemplatingUtil.replaceText(slide, values);
+            }
+            try (var outStream = new FileOutputStream(outputFilePath)) {
+                ppt.write(outStream);
+            }
+            ppt.close();
+        }
     }
 }
