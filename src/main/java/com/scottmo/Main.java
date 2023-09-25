d88592dd16885e4d395af829dc19ae2fd8c6e6ea
scottmo <scottscmo@gmail.com>
2023-09-24 22:49:13 -0500

    feat: spring webapp jar with server status in swing

--- src/main/java/com/scottmo/Main.java
+++ src/main/java/com/scottmo/Main.java
@@ -1,11 +1,35 @@
 package com.scottmo;
 
 import com.scottmo.app.App;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-public class Main {
+import javax.swing.*;
+
+@SpringBootApplication
+public class Main extends JFrame {
+    @Autowired
+    private ApplicationContext springContainer;
+
+    public Main() {
+        // Set up the frame
+        setTitle("Glory Presenter");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTextArea textArea = new JTextArea("Server running");
+
+        JPanel panel = new JPanel();
+        panel.add(textArea);
+        getContentPane().add(panel);
+
+        pack();
+        setVisible(true);
+    }
 
     public static void main(String[] args) {
-        // workaround to make fat jar executable
-        App.main(args);
+        SwingUtilities.invokeLater(Main::new);
+        SpringApplication.run(Main.class);
     }
 }
