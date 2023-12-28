ada4629fbbd8da298f535b1eb3610a47d6421c1e
scottmo <scottscmo@gmail.com>
2023-12-28 01:38:01 -0600

    refactor: data table cmp

---
+++ src/main/webapp/src/ui/components/DataTable.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+
+import { useDebouncedValue } from '@mantine/hooks';
+import { Table, TextInput, Container } from '@mantine/core';
+import '@mantine/core/styles/Container.css';
+import '@mantine/core/styles/Input.css';
+import '@mantine/core/styles/Table.css';
+
+type Column = {
+    label: string;
+}
+
+type Row = {
+    key: string;
+    columns: Column[];
+}
+
+type Props = {
+    headers: string[];
+    rows: Row[];
+}
+
+function stringMatch(term: string, text: string) {
+    return term.trim() ? text.toLowerCase().includes(term.trim().toLowerCase()) : true;
+}
+
+export default function DataTable({ headers, rows }: Props) {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [debouncedSearchTerm] = useDebouncedValue(searchTerm, 200);
+
+    const filteredRows = rows.filter(row => {
+        const searchKey = row.columns.reduce((acc, curr) => acc + " " + curr.label, "");
+        return stringMatch(debouncedSearchTerm, searchKey);
+    });
+
+    return (
+        <Container>
+            <TextInput placeholder="Search"
+                onChange={(event) => setSearchTerm(event.currentTarget.value)} />
+            <Table>
+                <Table.Thead>
+                    <Table.Tr>
+                    {headers.map(header => (
+                        <Table.Th>
+                            {header}
+                        </Table.Th>
+                    ))}
+                    </Table.Tr>
+                </Table.Thead>
+                <Table.Tbody>
+                {filteredRows.map(row => (
+                    <Table.Tr key={row.key}>
+                    {row.columns.map(column => (
+                        <Table.Td data-key={row.key + column.label}>
+                            {column.label}
+                        </Table.Td>
+                    ))}
+                    </Table.Tr>
+                ))}
+                </Table.Tbody>
+            </Table>
+        </Container>
+    );
+}
--- src/main/webapp/src/ui/containers/Songs.tsx
+++ src/main/webapp/src/ui/containers/Songs.tsx
@@ -1,19 +1,12 @@
 import { useState } from 'react';
-
-import { useDebouncedValue } from '@mantine/hooks';
-import { Table, LoadingOverlay, TextInput, Container } from '@mantine/core';
-import '@mantine/core/styles/Container.css';
-import '@mantine/core/styles/Table.css';
-import '@mantine/core/styles/Input.css';
 import { QueryAPI, useApi } from '../api';
 
-function stringMatch(term: string, text: string) {
-    return term.trim() ? text.toLowerCase().includes(term.trim().toLowerCase()) : true;
-}
+import { LoadingOverlay, Container } from '@mantine/core';
+import '@mantine/core/styles/Container.css';
+import '@mantine/core/styles/LoadingOverlay.css';
+import DataTable from '../components/DataTable';
 
 export default function Songs() {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [debouncedSearchTerm] = useDebouncedValue(searchTerm, 200);
     const { isPending, error, data } = useApi(QueryAPI.songList);
 
     if (isPending) return <LoadingOverlay visible={true} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />;
@@ -22,25 +15,12 @@ export default function Songs() {
 
     return (
         <Container>
-            <TextInput placeholder="Search" onChange={(event) => setSearchTerm(event.currentTarget.value)} />
-            <Table>
-                <Table.Thead>
-                    <Table.Tr>
-                        <Table.Th>Name</Table.Th>
-                    </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>
-                {
-                    Object.entries(data)
-                    .filter(([ _, songName ]) => stringMatch(debouncedSearchTerm, songName as string))
-                    .map(([ key, songName ]) => (
-                        <Table.Tr key={key}>
-                            <Table.Td data-key={key}>{songName as string}</Table.Td>
-                        </Table.Tr>
-                    ))
-                }
-                </Table.Tbody>
-            </Table>
+            <DataTable
+                headers={['Name']}
+                rows={Object.entries(data).map(([key, songName]) => ({
+                    key, columns: [{ label: songName }]
+                }))}
+            />
         </Container>
     );
 }
