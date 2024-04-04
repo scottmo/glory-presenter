c801bd7c886f441b5325cefae35031f57d11cbf7
scottmo <scottscmo@gmail.com>
2024-04-03 17:53:31 -0700

    fix duplicate template paths entries

--- src/main/java/com/scottmo/config/definitions/AppConfig.java
+++ src/main/java/com/scottmo/config/definitions/AppConfig.java
@@ -2,18 +2,20 @@ package com.scottmo.config.definitions;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public record AppConfig (
         String dataDir,
         List<String> locales,
         Map<String, String> bibleVersionToLocale,
-        List<String> templatePaths
+        Set<String> templatePaths
 ) {
     public AppConfig {
         if (locales == null) locales = new ArrayList<>();
         if (bibleVersionToLocale == null) bibleVersionToLocale = new HashMap<>();
-        if (templatePaths == null) templatePaths = new ArrayList<>();
+        if (templatePaths == null) templatePaths = new HashSet<>();
     }
 }
