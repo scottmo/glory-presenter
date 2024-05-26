8f4ff6f2532bb443a30258e85275fa684d0c5640
scottmo <scottscmo@gmail.com>
2024-05-26 17:22:51 -0500

    convert appconfig to java class

--- src/main/java/com/scottmo/config/definitions/AppConfig.java
+++ src/main/java/com/scottmo/config/definitions/AppConfig.java
@@ -7,15 +7,52 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-public record AppConfig (
-        String dataDir,
-        List<String> locales,
-        Map<String, String> bibleVersionToLocale,
-        Set<String> templatePaths
-) {
-    public AppConfig {
-        if (locales == null) locales = new ArrayList<>();
-        if (bibleVersionToLocale == null) bibleVersionToLocale = new HashMap<>();
-        if (templatePaths == null) templatePaths = new HashSet<>();
+public class AppConfig {
+    private String dataDir;
+    private List<String> locales;
+    private Map<String, String> bibleVersionToLocale;
+    private Set<String> templatePaths;
+
+    public AppConfig(
+            String dataDir,
+            List<String> locales,
+            Map<String, String> bibleVersionToLocale,
+            Set<String> templatePaths) {
+        this.dataDir = dataDir;
+        this.locales = (locales == null) ? new ArrayList<>() : locales;
+        this.bibleVersionToLocale = (bibleVersionToLocale == null) ? new HashMap<>() : bibleVersionToLocale;
+        this.templatePaths = (templatePaths == null) ? new HashSet<>() : templatePaths;
+    }
+
+    public String dataDir() {
+        return dataDir;
+    }
+
+    public void setDataDir(String dataDir) {
+        this.dataDir = dataDir;
+    }
+
+    public List<String> locales() {
+        return locales;
+    }
+
+    public void setLocales(List<String> locales) {
+        this.locales = locales;
+    }
+
+    public Map<String, String> bibleVersionToLocale() {
+        return bibleVersionToLocale;
+    }
+
+    public void setBibleVersionToLocale(Map<String, String> bibleVersionToLocale) {
+        this.bibleVersionToLocale = bibleVersionToLocale;
+    }
+
+    public Set<String> templatePaths() {
+        return templatePaths;
+    }
+
+    public void setTemplatePaths(Set<String> templatePaths) {
+        this.templatePaths = templatePaths;
     }
 }
